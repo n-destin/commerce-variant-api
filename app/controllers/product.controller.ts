fc0f95fe4a2b7fad11108bb0481342b86d63ed47
Eric Niyonkuru <eric.niyonkuru@pesachoice.com>
2023-10-31 23:38:39 +0200

    new feature for update product

--- app/controllers/product.controller.ts
+++ app/controllers/product.controller.ts
@@ -1,19 +1,31 @@
-import { Category } from "../database/Category";
-import { ICategory, ICreateCategory } from "../types/category.type";
-import { Route, Controller, Get, Tags, Security, Post, Body } from "tsoa";
+import { Route, Controller, Get, Tags, Security, Post, Body, Put, Path } from "tsoa";
 import { IProduct, IProductResponse, ProductDto } from "../types/product.type";
 import { Product } from "../database/Product";
 
 @Tags("Products")
 @Route("api/products")
 export class ProductController extends Controller {
- @Get("/")
- public static async getProducts(): Promise<IProductResponse[]> {
-  return await Product.find().populate(["category", "condition"]);
- }
- @Security("jwtAuth")
- @Post("/")
- public static async createProduct(@Body() product: ProductDto): Promise<IProduct> {
-  return (await Product.create(product)) as unknown as IProduct;
- }
+  @Get("/")
+  public static async getProducts(): Promise<IProductResponse[]> {
+    return await Product.find().populate(["category", "condition"]);
+  }
+
+  @Security("jwtAuth")
+  @Post("/")
+  public static async createProduct(@Body() product: ProductDto): Promise<IProduct> {
+    return (await Product.create(product)) as unknown as IProduct;
+  }
+
+  @Security("jwtAuth")
+  @Put("/{productId}")
+  public static async updateProduct(
+    @Path() productId: string,
+    @Body() product: ProductDto): Promise<IProduct> {
+    const updated = await Product.findOneAndUpdate(
+      { _id: productId },
+      { $set: product },
+      { new: true }
+    ) as IProduct;
+    return updated;
+  }
 }
--- app/routes/product.routes .ts
+++ app/routes/product.routes .ts
@@ -3,31 +3,46 @@ import express, { NextFunction, Request, Response } from "express";
 import passport from "passport";
 import { ProductController } from "../controllers/product.controller";
 import { IUser } from "../types/User.type";
+import { checkProduct } from "../middlewares/product.middleware";
 
 const productRouter = express.Router();
 
 productRouter.get("/", async (req: Request, res: Response, next: NextFunction) => {
- try {
-  const products = await ProductController.getProducts();
-  return res.status(200).json(products);
- } catch (error) {
-  return next(error);
- }
+  try {
+    const products = await ProductController.getProducts();
+    return res.status(200).json(products);
+  } catch (error) {
+    return next(error);
+  }
 });
 productRouter.post(
- "/",
- passport.authenticate("jwt", { session: false }),
- async (req: Request, res: Response, next: NextFunction) => {
-  try {
-   const user = req.user as IUser;
-   const createdProduct = await ProductController.createProduct({
-    ...req.body,
-    owner: user._id,
-   });
-   return res.status(201).json(createdProduct);
-  } catch (error) {
-   return next(error);
-  }
- },
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = req.user as IUser;
+      const createdProduct = await ProductController.createProduct({
+        ...req.body,
+        owner: user._id,
+      });
+      return res.status(201).json(createdProduct);
+    } catch (error) {
+      return next(error);
+    }
+  },
 );
+
+productRouter.put(
+  "/:productId", checkProduct,
+  passport.authenticate("jwt", { session: false }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const createdProduct = await ProductController.updateProduct(req.params.productId, req.body);
+      return res.status(200).json(createdProduct);
+    } catch (error) {
+      return next(error);
+    }
+  },
+);
+
 export default productRouter;
