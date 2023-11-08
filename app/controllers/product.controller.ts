c62120704a828f9408a5718568bdd0968f7e8683
Reponse Ashimwe <reponseashime@gmail.com>
2023-11-08 11:21:33 +0200

    Products CRUD

--- app/controllers/product.controller.ts
+++ app/controllers/product.controller.ts
@@ -1,4 +1,16 @@
-import { Route, Controller, Get, Tags, Security, Post, Body, Put, Path } from "tsoa";
+import {
+  Route,
+  Controller,
+  Get,
+  Tags,
+  Security,
+  Post,
+  Body,
+  Put,
+  Path,
+  Delete,
+  Inject,
+} from "tsoa";
 import { IProduct, IProductResponse, ProductDto } from "../types/product.type";
 import { Product } from "../database/Product";
 
@@ -6,8 +18,10 @@ import { Product } from "../database/Product";
 @Route("api/products")
 export class ProductController extends Controller {
   @Get("/")
-  public static async getProducts(): Promise<IProductResponse[]> {
-    return await Product.find().populate(["category", "condition"]);
+  public static async getProducts(
+    @Inject() condition: { [key: string]: string } = {},
+  ): Promise<IProductResponse[]> {
+    return await Product.find({ ...condition }).populate(["category", "condition"]);
   }
 
   @Security("jwtAuth")
@@ -17,15 +31,23 @@ export class ProductController extends Controller {
   }
 
   @Security("jwtAuth")
+  @Delete("{productId}")
+  public static async deleteProduct(@Path() productId: string): Promise<string> {
+    await Product.deleteOne({ _id: productId });
+    return "Deleted successful";
+  }
+
+  @Security("jwtAuth")
   @Put("/{productId}")
   public static async updateProduct(
     @Path() productId: string,
-    @Body() product: ProductDto): Promise<IProduct> {
-    const updated = await Product.findOneAndUpdate(
+    @Body() product: ProductDto,
+  ): Promise<IProduct> {
+    const updated = (await Product.findOneAndUpdate(
       { _id: productId },
       { $set: product },
-      { new: true }
-    ) as IProduct;
+      { new: true },
+    )) as IProduct;
     return updated;
   }
 }
--- app/middlewares/product.middleware.ts
+++ app/middlewares/product.middleware.ts
@@ -2,21 +2,24 @@ import { NextFunction } from "express";
 import { Request, Response } from "express";
 import { Product } from "../database/Product";
 
-export const checkProduct = async (req: Request, res: Response, next: NextFunction) => {
+export const checkProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   const id = req.params.productId as string;
   try {
-    const product = await Product.findById(id);
+    const product = await Product.findOne().or([{ _id: id }, { id: id }]);
 
     if (!product) {
       return res.status(400).json({
-        message: "No product not found "
+        message: "No product found ",
       });
     }
     next();
   } catch (error) {
     return res.status(500).json({
-      message: "An error occurred while checking the product"
+      message: "An error occurred while checking the product",
     });
   }
 };
-
